233c0d3a30efaa304cf4afd2a295c414d50151bf
Eron <eroncastro@gmail.com>
2017-07-11 14:17:16 -0300

    Adds Lecture 25

---
+++ Section_3/Lecture_25/app.js
@@ -0,0 +1,8 @@
+/*
+  The following require will look for greet.
+  If it does not find, it will search inside greet for index.js
+*/
+var greet = require('./greet');
+
+greet.english();
+greet.spanish();
