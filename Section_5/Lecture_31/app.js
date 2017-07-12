59310a2c1747c66f8c9da5c4e58860fdae723828
Eron <eroncastro@gmail.com>
2017-07-12 14:20:13 -0300

    Adds lectures 31 and 32

---
+++ Section_5/Lecture_31/app.js
@@ -0,0 +1,8 @@
+/*
+  Event: something that has happened in our app that
+         we can respond to.
+
+  In Node, we talk about two different kinds of events:
+  - System events: C++ Code - libuv;
+  - Custom events: JavaScript core - Event Emitter.
+*/
