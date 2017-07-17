d541364c5abf9263ec848fa50dcb62ac272eb12f
Eron <eroncastro@gmail.com>
2017-07-17 08:03:22 -0300

    Adds Lectures 42 and 43

---
+++ Section_6/Lecture_42/app.js
@@ -0,0 +1,7 @@
+/*
+  Asynchronous: more than one process running simultaneously.
+  Node does things asynchronously. V8 does not.
+
+  Synchronous: one process executing at a time.
+  JS is synchronous; NodeJS is asynchronous.
+*/
---
+++ Section_6/Lecture_43/app.js
@@ -0,0 +1,4 @@
+/*
+  Callbacks: a function passed to some other function
+  which we assume will be invoked at some point in time.
+*/
