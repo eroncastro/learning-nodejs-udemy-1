08d9fae49645abcf68ee428f5de3e9bf8111a01b
Eron <eroncastro@gmail.com>
2017-07-17 14:23:33 -0300

    Adds Lecture 50

---
+++ Section_6/Lecture_50/app.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+var greet = fs.readFileSync(`${__dirname}/greet.txt`, 'utf8');
+console.log(greet);
+
+/*
+  Error-first callback: callbacks take an error object
+  as their first parameter. They are null if no error,
+  otherwise will contain an object defining the error.
+*/
+
+fs.readFile(`${__dirname}/greet.txt`, function(err, data) {
+  if (!err) console.log(data.toString());
+});
+
+fs.readFile(`${__dirname}/greet.txt`, 'utf8', function(err, data) {
+  if (!err) console.log(data.toString());
+});
+
+
