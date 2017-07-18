4a82b928f6f06fb3dd5e8b1381240ce65229a81b
Eron Castro <eroncastro@gmail.com>
2017-07-17 23:10:40 -0300

    Adds Lecture 45

---
+++ Section_6/Lecture_45/app.js
@@ -0,0 +1,9 @@
+/*
+  Streams and buffers
+
+  Buffer: a temporary holding spot for data being moved from one place
+  to another. It is intentionally limited in size.
+
+  Stream: a sequence of data being available over time. Pieces of data
+  that eventually combine into a whole.
+*/
