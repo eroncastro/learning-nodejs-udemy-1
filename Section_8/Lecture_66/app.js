c7f8355af99cb1b0618480f452ced62bfc0b1f16
Eron <eroncastro@gmail.com>
2017-07-18 17:21:21 -0300

    Adds Lectures 66 and 67

---
+++ Section_8/Lecture_66/app.js
@@ -0,0 +1,9 @@
+/*
+  Package - collection of code managed and maintained by a package manager.
+
+  Package management system - software that automates installing and updating
+                              packages. Deals with what version you have or need
+                              and manages dependencies.
+
+  Dependency - code that another set of code depends on to function.
+*/
