0fdae63490ecef23daed60b00a649469b08a6757
Eron Castro <eroncastro@gmail.com>
2017-07-17 23:10:52 -0300

    Adds Lecture 46

---
+++ Section_6/Lecture_46/app.js
@@ -0,0 +1,9 @@
+/*
+  Binary data - data stored in binary.
+
+  Character set - a representation of characters as numbers.
+  Each character gets a number. Unicode and ASCII are character sets.
+
+  Character encoding - how characters are stored in binary.
+  The numbers (or code points) are converted and stored in binary.
+*/
