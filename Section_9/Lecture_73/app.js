0ed90409b7f9197795ae85b6016b112ae8f7d52b
Eron Castro <eroncastro@gmail.com>
2017-07-18 23:39:37 -0300

    Adds Lecture 73

---
+++ Section_9/Lecture_73/app.js
@@ -0,0 +1,19 @@
+/*
+  Environment variables - variables specific to the environment
+                          our code is living in.
+*/
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+});
+
+app.get('/api', function(req, res) {
+  res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port);
