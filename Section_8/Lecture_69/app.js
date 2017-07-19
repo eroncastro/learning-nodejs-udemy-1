54785aa8f1e89a62863693160e5cf66a806aa584
Eron Castro <eroncastro@gmail.com>
2017-07-18 21:13:39 -0300

    Adds Lecture 69

---
+++ Section_8/Lecture_69/app.js
@@ -0,0 +1,10 @@
+/*
+  init, nodemon and package.json
+
+  ^ -> only minor and patch updates are allowed.
+  ~ -> only patch updates are allowed.
+*/
+
+var moment = require('moment');
+
+console.log(moment().format('YYYY-MM-DD'));
