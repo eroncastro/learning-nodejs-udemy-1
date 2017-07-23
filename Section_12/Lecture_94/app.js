470806ba5ada18bd9d0e986cd55de686cd1daa0e
Eron Castro <eroncastro@gmail.com>
2017-07-23 10:57:32 -0300

    Fixes Lecture 93 and adds Lecture 94

--- Section_12/Lecture_94/app.js
+++ Section_12/Lecture_94/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var config = require('./config');
 var mongoose = require('mongoose');
+var setupController = require('./controllers/setupController');
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -10,5 +11,7 @@ app.use('/assets', express.static(`${__dirname}/public`));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
+
+setupController(app);
 
 app.listen(port);
---
+++ Section_12/Lecture_94/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  getDbConnectionString: function() {
+    return `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}` +
+      '@ds115583.mlab.com:15583/testing';
+  }
+};
